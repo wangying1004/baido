e21e204f12cbb5871d268953bdf4dc6006fac605
wangying <wwwww@mail.com>
2016-09-22 20:56:33 +0800

    改变图片

--- 1.js
+++ 1.js
@@ -15,4 +15,43 @@
             erweima.style.display = "block";
         }
     }
+    var photoschange = document.getElementById('photoschange');
+    var click1_photoschange = document.getElementById('click1_photoschange');
+    var click_photoschange = document.getElementById('cp');
+    click1_photoschange.onclick = function() {
+        if (photoschange.src.match("3.jpg")) {
+            photoschange.src = "9.jpg";
+        } else if (photoschange.src.match("9.jpg")) {
+            photoschange.src = "8.jpg";
+        } else if (photoschange.src.match("8.jpg")) {
+            photoschange.src = "7.jpg";
+        } else if (photoschange.src.match("7.jpg")) {
+            photoschange.src = "6.jpg";
+        } else if (photoschange.src.match("6.jpg")) {
+            photoschange.src = "5.jpg";
+        } else if (photoschange.src.match("5.jpg")) {
+            photoschange.src = "4.jpg";
+        } else if (photoschange.src.match("4.jpg")) {
+            photoschange.src = "3.jpg";
+        }
+    }
+
+    click_photoschange.onclick = function() {
+        if (photoschange.src.match("3.jpg")) {
+            photoschange.src = "4.jpg";
+        } else if (photoschange.src.match("4.jpg")) {
+            photoschange.src = "5.jpg";
+        } else if (photoschange.src.match("5.jpg")) {
+            photoschange.src = "6.jpg";
+        } else if (photoschange.src.match("6.jpg")) {
+            photoschange.src = "7.jpg";
+        } else if (photoschange.src.match("7.jpg")) {
+            photoschange.src = "8.jpg";
+        } else if (photoschange.src.match("8.jpg")) {
+            photoschange.src = "9.jpg";
+        } else if (photoschange.src.match("9.jpg")) {
+            photoschange.src = "3.jpg";
+        }
+    }
+
 }(window))
